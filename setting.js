276fd8fb7bb26befb373ce510143a96c7a13bfdf
gojo7668 <nnarutouzumaki25000@gmail.com>
2025-05-16 22:12:57 +0530

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+
+module.exports = {
+
+SESSION_ID: process.env.SESSION_ID === undefined ? 'GOJO MD=TE93kCJA#jr9AOX1sBvwKX3WZL5PIaZjkaBDLxUe9smZcqQtbfns' : process.env.SESSION_ID, 
+PORT: process.env.PORT === undefined ? '8000' : process.env.PORT, 
+SESSION_NAME: process.env.SESSION_NAME === undefined ? 'movievisper' : process.env.SESSION_NAME,  
+
+};
