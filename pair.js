3d2c1c14d6af6ac688cd47d2398971125fb6da1c
gojo7668 <nnarutouzumaki25000@gmail.com>
2025-05-17 07:48:26 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -72,7 +72,7 @@ router.get('/', async (req, res) => {
                         const dt = await PrabathPairWeb.sendMessage(user_jid, {
                          image: { 
                           url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
-              },
+                         },
                 
               
                         });
