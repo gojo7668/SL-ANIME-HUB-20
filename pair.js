3fa4bb67793c3b6640349d434509316391294b82
gojo7668 <nnarutouzumaki25000@gmail.com>
2025-05-17 07:44:03 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -70,7 +70,11 @@ router.get('/', async (req, res) => {
                         const sid = "GOJO MD=" + string_session;
 
                         const dt = await PrabathPairWeb.sendMessage(user_jid, {
-                            text: sid
+                                    image: { 
+                                      url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+              },
+                
+              
                         });
 
                                    await PrabathPairWeb.sendMessage(user_jid, {
